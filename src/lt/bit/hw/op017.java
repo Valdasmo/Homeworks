e9d5aa67640d1d9cbc56b1f4bafea11fe117ede6
Valdasmo <51168355+Valdasmo@users.noreply.github.com>
2019-07-25 01:45:23 +0300

    2019-07-24

--- src/lt/bit/hw/op017.java
+++ src/lt/bit/hw/op017.java
@@ -4,7 +4,6 @@ public class op017 {
     public static void main(String args[]) {
         int a, b;
 
-        // first print the top header row
         System.out.format("      ");
         for (int i = 1; i <= 10; i++) {
             System.out.format("%4d", i);
@@ -13,6 +12,7 @@ public class op017 {
         System.out.println("----------------------------------------------");
 
         for (a = 1; a <= 10; a++) {
+            System.out.format("%4d |", a);
             for (b = 1; b <= 10; b++) {
                 System.out.format("%4d", a * b);
             }
