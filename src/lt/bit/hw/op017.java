3126fe41ab00055c206397db3b0cb9e937bbd27b
Valdasmo <51168355+Valdasmo@users.noreply.github.com>
2019-07-24 19:39:35 +0300

    2019-07-24

--- src/lt/bit/hw/op017.java
+++ src/lt/bit/hw/op017.java
@@ -2,9 +2,16 @@ package lt.bit.hw;
 
 public class op017 {
     public static void main(String args[]) {
+        int a, b;
 
-        int a;
-        int b;
+        // first print the top header row
+        System.out.format("      ");
+        for (int i = 1; i <= 10; i++) {
+            System.out.format("%4d", i);
+        }
+        System.out.println();
+        System.out.println("----------------------------------------------");
+
         for (a = 1; a <= 10; a++) {
             for (b = 1; b <= 10; b++) {
                 System.out.format("%4d", a * b);
