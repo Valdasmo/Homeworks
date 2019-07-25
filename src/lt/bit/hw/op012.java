365061ac2b8d130cd5447334c2521a2c8591f038
Valdasmo <51168355+Valdasmo@users.noreply.github.com>
2019-07-25 20:41:20 +0300

    2019-07-25

---
+++ src/lt/bit/hw/op012.java
@@ -0,0 +1,24 @@
+package lt.bit.hw;
+
+import java.util.Arrays;
+
+//public class op012 {
+//    public static void main(String[] args) {
+//        insertElementInArray();
+//    }
+//    private static void insertElementInArray() {
+//        int [] numbers = {1,2,3,4,5,6,7,7,8,8,0};
+//        int index = 2;
+//        System.out.println(numbers.length);
+//        int[] newNumbers = new int[numbers.length + 1];
+//        for (int  i = 0; i < index; i++){
+//            newNumbers[i] = numbers[i];
+//        }
+//        newNumbers[index] = 100;
+//        for (int  i = index + 1; i < newNumbers.length; i++){
+//            newNumbers[i] = numbers[i - 1];
+//        }
+//        System.out.println(Arrays.toString(numbers));
+//        System.out.println(Arrays.toString(newNumbers));
+//    }
+//}
