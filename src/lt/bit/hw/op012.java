638f1421c662994dd61e60c1b050808c629014b5
Valdasmo <51168355+Valdasmo@users.noreply.github.com>
2019-07-25 21:35:06 +0300

    2019-07-25

--- src/lt/bit/hw/op012.java
+++ src/lt/bit/hw/op012.java
@@ -1,4 +1,15 @@
 package lt.bit.hw;
 
 public class op012 {
+    public static void main(String[] args) {
+    int length = 10;
+    int width = 5;
+            areaCalculiator(length, width);
+    }
+
+    private static void areaCalculiator(int length, int width) {
+
+    }
+
+
 }
