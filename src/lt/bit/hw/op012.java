7e47f0cf38788fdb5a76c0512d2bc7e131cb463d
Valdasmo <51168355+Valdasmo@users.noreply.github.com>
2019-07-26 00:40:16 +0300

    2019-07-25

--- src/lt/bit/hw/op012.java
+++ src/lt/bit/hw/op012.java
@@ -2,14 +2,22 @@ package lt.bit.hw;
 
 public class op012 {
     public static void main(String[] args) {
-    int length = 10;
-    int width = 5;
-            areaCalculiator(length, width);
+        int length = 10, width = 5, radius = 5;
+        Double edgeOne = 20.0, edgeTwo = 10.0;
+        areaCalculiator(length, width);
+        areaCalculiator(edgeOne, edgeTwo);
+        areaCalculiator(radius);
     }
 
     private static void areaCalculiator(int length, int width) {
-
+        System.out.println("Rectangle surface area: " + length * width);
     }
 
+    private static void areaCalculiator(double edgeOne, double edgeTwo) {
+        System.out.println("Right triangle surface area: " + edgeOne * edgeTwo / 2);
+    }
+    private static void areaCalculiator(int radius) {
+        System.out.println("Circle surface area: " + Math.PI * radius * radius);
+    }
 
 }
