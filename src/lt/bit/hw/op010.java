aacaa62b7cf46fbe71ac5ae8664308e2744febee
Valdasmo <51168355+Valdasmo@users.noreply.github.com>
2019-07-25 20:42:35 +0300

    2019-07-25

---
+++ src/lt/bit/hw/op010.java
@@ -0,0 +1,28 @@
+package lt.bit.hw;
+
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class op010 {
+    public static void main(String[] args) {
+        arraySorting();
+
+    }
+
+    private static void arraySorting() {
+        int[] numArray = new int[5];
+        for (int i = 0; i < numArray.length; i++) {
+            numArray[i] = (int) (Math.random() * 100);
+
+        }
+        System.out.println(Arrays.toString(numArray));
+        Arrays.sort(numArray);
+        System.out.println(Arrays.toString(numArray));
+        int arraySum = 0;
+        for (int i = 0; i<numArray.length; i++ ){
+        arraySum +=numArray[i];
+        }
+        System.out.println(arraySum);
+    }
+}
