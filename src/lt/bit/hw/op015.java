49c0943955c6c1a9e4f8926729a2bda8c1dc5041
Valdasmo <51168355+Valdasmo@users.noreply.github.com>
2019-07-29 17:59:57 +0300

    2019-07-25

--- src/lt/bit/hw/op015.java
+++ src/lt/bit/hw/op015.java
@@ -1,4 +1,16 @@
 package lt.bit.hw;
 
 public class op015 {
+    public static void main(String[] args) {
+        int y  = 0;
+        calcFunction(y);
+    }
+
+    private static void calcFunction(int y) {
+        System.out.println("y = x * 3");
+    for (int x=0; x<=10; x++){
+        System.out.println("When x = " + x+ " - Y = " + (x*3));
+    }
+
+    }
 }
